075b6895def79275653434c0728d4f4b8f5f2d5b
Erinna Chen <erinna.chen@gmail.com>
2015-12-16 20:21:00 -0700

    Refactored version of CompleteMe with tests

--- lib/complete_me.rb
+++ lib/complete_me.rb
@@ -14,25 +14,16 @@ class CompleteMe
   end
 
   def suggest(word_begin)
-    word_begin = word_begin.chars
     root.suggest(word_begin)
   end
-  #
-  # def find_node_to_search(word_begin)
-  #   node = root
-  #   word_begin.chars.each do |c|
-  #     node = node.paths[c]
-  #   end
-  #   node
-  # end
-  #
-  # def populate(dictionary)
-  #   dictionary.split("\n").each do |word|
-  #     insert(word)
-  #   end
-  # end
-  #
-  # def select(substring, word_to_select)
-  #   root.select(substring, word_to_select)
-  # end
+
+  def populate(dictionary)
+    dictionary.split("\n").each do |word|
+      insert(word)
+    end
+  end
+  
+  def select(substring, word_to_select)
+    root.select(substring, word_to_select)
+  end
 end
